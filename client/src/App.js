40bb3e8f66074136f7655baa93761bef2c00903f
Chimoha31 <mokochii1108@gmail.com>
2022-06-16 14:02:09 -0700

    Footer

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.scss';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import GuestRoom from './components/home/GuestRoom';
 import RoomCarousel from './components/home/RoomCarousel';
+import SearchForBook from './components/home/SearchForBook';
+import Service from './components/home/Service';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <RoomCarousel />
+      <SearchForBook />
+      <GuestRoom />
+      <Service />
       <Footer />
     </div>
   )
--- client/src/components/home/GuestRoom.jsx
+++ client/src/components/home/GuestRoom.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GuestRoom = () => {
   return (
     <div>
-      
+      Guest Room
     </div>
   )
 }
