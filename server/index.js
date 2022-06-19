39d913f5854c546fc3c55d4b5b66684839ec0373
Chimoha31 <mokochii1108@gmail.com>
2022-06-19 00:54:33 -0700

    Set mongo db to connect

--- server/index.js
+++ server/index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const mongoose = require("mongoose");
 const UserRoutes = require("./routes/user");
+require("dotenv").config();
 const PORT = process.env.PORT || 2000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// Connect MongoDB
+const mongo_URL = process.env.MONGDB_URL;
+mongoose.connect(mongo_URL, (err) => {
+  if(err) {
+    console.log("Failed to connect to MongoDB..")
+  }else{
+    console.log("Succesfully, connected to MongoDB 🎉")
+  }
+})
 
 app.get("/", (req, res) => {
   res.send("This is endpoint");
