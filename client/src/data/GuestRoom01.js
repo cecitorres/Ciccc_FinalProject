65db3a919df5bd8c4e6faf3f55abd8fbb752c817
Chimoha31 <mokochii1108@gmail.com>
2022-06-20 00:51:16 -0700

    Modal

--- client/src/data/GuestRoom01.js
+++ client/src/data/GuestRoom01.js
@@ -20,27 +20,6 @@ let guestRoom01 = [
     capacity: 4,
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur eveniet vitae autem laudantium eius quia fugiat, nulla delectus perferendis iure sint accusantium enim officiis optio in veniam numquam aspernatur amet!",
   },
-  // {
-  //   id: 4,
-  //   image: "https://images.unsplash.com/photo-1591088398332-8a7791972843?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80",
-  //   roomType: "King Room",
-  //   capacity: 2,
-  //   description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur eveniet vitae autem laudantium eius quia fugiat, nulla delectus perferendis iure sint accusantium enim officiis optio in veniam numquam aspernatur amet!",
-  // },
-  // {
-  //   id: 5,
-  //   image: "https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2231&q=80",
-  //   roomType: "Family Room",
-  //   capacity: 6,
-  //   description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur eveniet vitae autem laudantium eius quia fugiat, nulla delectus perferendis iure sint accusantium enim officiis optio in veniam numquam aspernatur amet!",
-  // },
-  // {
-  //   id: 6,
-  //   image: "https://images.unsplash.com/photo-1578683010236-d716f9a3f461?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
-  //   roomType: "Family Room",
-  //   capacity: 5,
-  //   description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequuntur eveniet vitae autem laudantium eius quia fugiat, nulla delectus perferendis iure sint accusantium enim officiis optio in veniam numquam aspernatur amet!",
-  // },
 ]
 
 export default guestRoom01;
